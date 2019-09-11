b7149789c2eaedde32d980124444c51a151c2ab5
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-11 23:38:51 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,13 @@
 class AuthorsController < ApplicationController
 
+  def show
+    @author = Author.find(params[:id])
+  end
+
   def new
     @author = Author.new
   end
-  
+
   def index
     @authors = Author.all
   end
