99760221c4a80fb516948d743e8bacdcfd229efe
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-11 23:58:55 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,4 @@
 module PostsHelper
-  
   def author_id_field(post)
     if post.author.nil?
       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
@@ -7,5 +6,4 @@ module PostsHelper
       hidden_field_tag "post[author_id]", post.author_id
     end
   end
-
 end
