b0b9a929c3f6308abda2d3f82e2c4dfb6b68d160
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-11 23:59:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,11 +18,11 @@ class PostsController < ApplicationController
 
   def edit
     if params[:author_id]
-      @author = Author.find_by(id: params[:author_id])
-      if @author.nil?
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
         redirect_to authors_path, alert: "Author not found."
       else
-        @post = author.posts.find_by(params[:author_id])
+        @post = author.posts.find_by(id: params[:id])
         redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
       end
     else
